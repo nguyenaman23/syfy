52730f0ff42e778e057ab1de381277c19797268a
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-13 07:57:29 +0700

    Add files via upload

---
+++ syfy/config.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+$useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36';
+
+
+
+$cookie = "Referral_Source=https%3A%2F%2Fchat.zalo.me%2F; _ga=GA1.2.1764595912.1634084171; _gid=GA1.2.883009094.1634084171; HstCfa4527147=1634084171940; HstCmu4527147=1634084171940; c_ref_4527147=https%3A%2F%2Fchat.zalo.me%2F; HstCnv4527147=1; HstCns4527147=1; __dtsu=6D001632281942D39E6C1D3947196DF0; _cc_id=843d098d4edaccc383297862c6cabc43; panoramaId_expiry=1634688995159; panoramaId=a806c8199c721b26f48e7c6a8d174945a702486465cb10ccb82f35cfccb49422; __gads=ID=144599bcbb0e76fb-2272cf6b6ecc00be:T=1634084204:RT=1634084204:S=ALNI_Ma0d9WbSVfP74PT28PZlRBbqhGf5Q; csrf_cookie_name=7c63b9ef73dfdc3b81767ac376a84784; _data_cpm=11-1_111-1; _data_html=11-1_35-1_55-1_111-1; a=N4IePwXSTA7PxsH3dVKDEBaTc7ZZuOfr; token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYWYl6gFhZiYGgAGBAsAAIOYqu6Dcj2t5pXrv0vdgy0RtJlF63CgL7KdJxFX3UU3xwQBHMEUCICf-x75FUHxxlonVmYpDXHgOG_aHsEeMusXVnt_vKyyhAiEA8BX1Bj5AaS3_6nkjTqn973d7qcqsMpX1XlqWp9GgVDc; _popfired=2; _popfired_expires=Invalid%20Date; lastOpenAt_=1634084356960; _data_cpc=67-1_69-2_115-3; ci_session=0c5d941e5bff409480a681aaab27c0b58f7508e4; HstCla4527147=1634084465958; HstPn4527147=18; HstPt4527147=18";
+
+
+
+
+
+
+
